4765818d5995ee7a561b548afe383be96bb1a41a
IsabelleVolaco <isabelletvolaco@gmail.com>
2025-03-05 14:55:17 -0300

    await

--- backend/doceria_backend.dart
+++ backend/doceria_backend.dart
@@ -13,6 +13,11 @@ void main() {
   try{
     var conn = await MySqlConnection.connect(settings);
     print('banco conectou');
+
+    await Future.delayed(Duration(seconds: 5));
+    await conn.close();
+    print('banco fechou');
+
   } catch (erro) {
     print('deu errado');
   }
